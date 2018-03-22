775a64b09505b76ba7bb0cb30f1625d74b881a5f
romain <rboitard@gmail.com>
2018-03-22 16:31:32 +0100

    update adult.js

--- common/models/adult.js
+++ common/models/adult.js
@@ -16,8 +16,6 @@ module.exports = function(Adult) {
   Adult.disableRemoteMethodByName('replaceOrCreate');
   Adult.disableRemoteMethodByName('upsertWithWhere');
 
-
-
   Adult.remoteMethod(
     'Sum', {
       description: 'sum request',
@@ -25,4 +23,19 @@ module.exports = function(Adult) {
       accepts: [{arg: 'arg1', type: 'any'}],
       returns: {arg: 'response', type: 'any'}}
   );
+};
+
+var sgn = function(x) {
+  return x < 0 ? -1 : 1;
+};
+
+// From wikipedia:
+// Lap(X) = mu - b sgn(U) ln (1-2|U|) where U is a random variable between -0.5 and 0.5
+var laplace = function(mu, b) {
+  var U = Math.random() - 0.5;
+  return mu - (b * sgn(U) * Math.log(1 - 2 * Math.abs(U)));
+};
+
+var privatieze = function(F, deltaF, epsilon) {
+  return F + laplace(0.0, deltaF / epsilon);
 };
